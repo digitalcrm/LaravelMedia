05e5762759b96ccd391c32a1ba7e40908dac1922
digitalcrm <info@digitalcrm.com>
2019-09-27 14:27:13 +0530

    Accessing profie tag continue
    Add bootstrap file in profile views and create resource controller

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -3,8 +3,23 @@
 
 @section('content')
 
-
-<h1>Hello nick</h1>
+<div class="container">
+	<div class="row">
+		<form action="{{ route('avatar.store') }}" method="POST" enctype="multipart/form-data">
+			@csrf
+			<div class="input-group mb-3">
+				<div class="input-group-prepend">
+					<span class="input-group-text" id="inputGroupFileAddon01">Upload</span>
+				</div>
+				<div class="custom-file">
+					<input type="file" name="avatar" class="custom-file-input" id="inputGroupFile01" aria-describedby="inputGroupFileAddon01">
+					<label class="custom-file-label" for="inputGroupFile01">Choose file</label>
+				</div>
+				<input type="submit" value="Upload" class="btn btn-success" ml-4>
+			</div>
+		</form>
+	</div>
+</div>
 
 @endsection()
 
--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,16 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
 });
+
 //syntax for accessing view file without controller Route::view('urlname','views file name'); 
 Route::view('profile','profile')->name('profile');
+
+//create resource controller
+Route::resource('avatar','AvatarController');
+
 
 Auth::routes();
 
