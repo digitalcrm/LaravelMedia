cfd9b695b57f7c8fe0d264cdb2a9c0ec39153330
Neeraj Singh <43599636+Neeraj1005@users.noreply.github.com>
2020-10-02 09:57:01 +0530

    Symfony 5 Related Upgrades

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,7 @@
 
 namespace App\Exceptions;
 
-use Exception;
+use Throwable;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
@@ -44,7 +44,7 @@ class Handler extends ExceptionHandler
      * @param  \Exception  $exception
      * @return \Illuminate\Http\Response
      */
-    public function render($request, Exception $exception)
+    public function render($request, Throwable $exception)
     {
         if ($exception instanceof FileUnacceptableForCollection) {
             return redirect()->back()->with('error','Only jpeg file accepted here');
